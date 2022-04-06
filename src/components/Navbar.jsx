01023c1d9246a0aa15ed9fbc0559c321067f8e9a
megha083 <meghamathuria83@gmail.com>
2022-04-06 19:47:31 +0530

    pizzasapp

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+        <nav className="navbar navbar-light bg-light shadow-lg p-3 mb-5 bg-body rounded">
+  <div className="container-fluid">
+    <a className="navbar-brand">PIZZAS</a>
+    
+  </div>
+</nav>
+    </div>
+  )
+}
+
---
+++ src/pizzasdata.jsx
@@ -0,0 +1,201 @@
+const pizzas=[
+{
+    "name": "PEPPER BARBECUE CHICKEN",
+    "varients": ["small","medium","large"],
+    "prices": [
+        {
+            "small": 200,
+            "medium": 350,
+            "large": 400
+        },
+    ],
+    "category": "nonveg",
+    "image": "https://www.dominos.co.in//files/items/Pepper_Barbeque.jpg",
+    "description": "Pepper Barbecue Chicken I Cheeze"
+}, 
+{
+
+    "name": "Non Veg Supreme",
+    "varients": [
+        "small",
+        "medium",
+        "large"
+    ],
+    "prices": [
+        {
+            "small": 200,
+            "medium": 350,
+            "large": 400
+        }
+    ],
+    "category": "nonveg",
+    "image": "https://www.dominos.co.in//files/items/Non-Veg_Supreme.jpg",
+    "description": "Non Veg Supreme Pizza | Cheeze"
+},
+{
+    "name": "Chicken Sausage",
+    "varients": [
+        "small",
+        "medium",
+        "large"
+    ],
+    "prices": [
+        {
+            "small": 200,
+            "medium": 350,
+            "large": 400
+        }
+    ],
+    "category": "nonveg",
+    "image": "https://www.dominos.co.in/files/items/Chicken_Sausage.jpg",
+    "description": "Chicken Sausage | Cheese"   
+},
+{
+    "name": "Chicken Golden Delight",
+    "varients": [
+        "small",
+        "medium",
+        "large"
+    ],
+    "prices": [
+        {
+            "small": 200,
+            "medium": 350,
+            "large": 400
+        }
+    ],
+    "category": "nonveg",
+    "image": "https://www.dominos.co.in/files/items/Chicken_Golden_Delight.jpg",
+    "description": "Mmm! Barbeque chicken with a topping of golden corn loaded with extra cheese. Worth its weight in gold!" 
+},
+{
+    "name": "Chicken Dominator",
+    "varients": [
+        "small",
+        "medium",
+        "large"
+    ],
+    "prices": [
+        {
+            "small": 200,
+            "medium": 350,
+            "large": 400
+        }
+    ],
+    "category": "nonveg",
+    "image": "https://www.dominos.co.in/files/items/Dominator.jpg",
+    "description": "Treat your taste buds with Double Pepper Barbecue Chicken, Peri-Peri Chicken, Chicken Tikka & Grilled Chicken Rashers" 
+},
+{
+    "name": "Pepper Barbecue & Onion",
+    "varients": [
+        "small",
+        "medium",
+        "large"
+    ],
+    "prices": [
+        {
+            "small": 200,
+            "medium": 350,
+            "large": 400
+        }
+    ],
+    "category": "nonveg",
+    "image": "https://www.dominos.co.in/files/items/Pepper_Barbeque_&_Onion.jpg",
+    "description": "Pepper Barbecue Chicken | Onion" 
+},
+{
+    "name": "MARGHERITA",
+    "varients": [
+        "small",
+        "medium",
+        "large"
+    ],
+    "prices": [
+        {
+            "small": 200,
+            "medium": 350,
+            "large": 400
+        }
+    ],
+    "category": "veg",
+    "image": "https://www.dominos.co.in/files/items/Margherit.jpg",
+    "description": "A hugely popular margherita, with a deliciously tangy single cheese topping"
+},
+{
+    "name": "DOUBLE CHEESE MARGHERITA",
+    "varients": [
+        "small",
+        "medium",
+        "large"
+    ],
+    "prices": [
+        {
+            "small": 200,
+            "medium": 350,
+            "large": 400
+        }
+    ],
+    "category": "veg",
+    "image": "https://www.dominos.co.in/files/items/Double_Cheese_Margherita.jpg",
+    "description": "The ever-popular Margherita - loaded with extra cheese... oodies of it!"
+},
+{
+    "name": "FARM HOUSE",
+    "varients": ["small","medium","large"],
+    "prices": [
+        {
+            "small": 200,
+            "medium": 350,
+            "large": 400
+        }
+    ],
+    "category": "veg",
+    "image": "https://www.dominos.co.in/files/items/Farmhouse.jpg",
+    "description": "A pizza that goes ballistic on veggies! Check out this mouth watering overload of crunchy, crisp capsicum, succulent mushrooms and fresh tomatoes"
+},
+{
+    "name": "PEPPY PANEER",
+    "varients": ["small","medium","large"],
+    "prices": [
+        {
+            "small": 200,
+            "medium": 350,
+            "large": 400
+        }
+    ],
+    "category": "veg",
+    "image": "https://www.dominos.co.in/files/items/Peppy_Paneer.jpg",
+    "description": "Chunky paneer with crisp capsicum and spicy red pepper - quite a mouthful!"
+},
+{
+    "name": "MEXICAN GREEN WAVE",
+    "varients": ["small","medium","large"],
+    "prices": [
+        {
+            "small": 200,
+            "medium": 350,
+            "large": 400
+        }
+    ],
+    "category": "veg",
+    "image": "https://www.dominos.co.in/files/items/Mexican_Green_Wave.jpg",
+    "description": "A pizza loaded with crunchy onions, crisp capsicum, juicy tomatoes and jalapeno with a liberal sprinkling of exotic Mexican herbs."
+},
+{
+    "name": "DELUXE VEGGIE",
+    "varients": ["small","medium","large"],
+    "prices": [
+        {
+            "small": 200,
+            "medium": 350,
+            "large": 400
+        }
+    ],
+    "category": "veg",
+    "image": "https://www.dominos.co.in/files/items/Deluxe_Veggie.jpg",
+    "description": "For a vegetarian looking for a BIG treat that goes easy on the spices, this one's got it all.. The onions, the capsicum, those delectable mushrooms - with paneer and golden corn to top it all."
+}
+]
+
+export default pizzas
